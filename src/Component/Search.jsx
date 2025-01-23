988d41b898d1ce85b46c18004a70d36bdaf72e6b
saumya2611 <somu0438@gmail.com>
2025-01-23 23:09:57 +0530

    add searching and suggestion

--- src/Component/Search.jsx
+++ src/Component/Search.jsx
@@ -1,15 +1,37 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 import { callApi } from "../Utils/CallApi";
+import { createSearchParams, useNavigate } from "react-router-dom";
 
 const Search = () => {
   const [suggestion, setSuggestion] = useState(null);
   const [searchterm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("All");
+
+  //   useEffect(() => {
+  //     console.log("suggestion===============>", suggestion);
+  //   }, [suggestion]);
+
+  const navigate = useNavigate();
 
   const getSuggestions = () => {
     callApi(`data/suggestions.json`).then((suggestionsResults) => {
+      //   console.log("suggestionsResults===========>", suggestionsResults);
       setSuggestion(suggestionsResults);
     });
+  };
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    navigate({
+      pathname: "search",
+      search: `${createSearchParams({
+        category: `${category}`,
+        searchterm: `${searchterm}`,
+      })}`,
+    });
+    setSearchTerm("");
+    setCategory("All");
   };
 
   useEffect(() => {
@@ -19,7 +41,10 @@ const Search = () => {
   return (
     <div className="w-[100%]">
       <div className="flex items-center h-10 bg-searchclone border-2 border-white rounded  hover:border-orange-400">
-        <select className="p-[7px] py-2 bg-gray-300 text-black text-xs xl:text-sm focus:outline-none">
+        <select
+          className="p-[7px] py-2 bg-gray-300 text-black text-xs xl:text-sm focus:outline-none"
+          onChange={(ev) => setCategory(ev.target.value)}
+        >
           <option>All</option>
           <option>Deals</option>
           <option>Amazon</option>
@@ -35,7 +60,7 @@ const Search = () => {
           value={searchterm}
           onChange={(ev) => setSearchTerm(ev.target.value)}
         />
-        <button className="w-[45px]">
+        <button onClick={onHandleSubmit} className="w-[45px]">
           <MagnifyingGlassIcon className="h-[27px] m-auto stroke-slate-900" />
         </button>
       </div>
@@ -43,15 +68,35 @@ const Search = () => {
         <div className="bg-white text-black w-full z-40 absolute">
           {suggestion
             .filter((suggestion) => {
-              const currentSearchTerm = searchterm.toLocaleLowerCase();
-              const title = suggestion.title.toLocaleLowerCase();
+              const currentSearchTerm = searchterm.toLowerCase();
+              //   console.log("currentSearchTerm==========>", currentSearchTerm);
+
+              const title = suggestion.title.toLowerCase();
+              //    console.log("title==========>", title);
+
+              //   console.log(
+              //     `currentSearchTerm &&
+              //     title.startsWith(currentSearchTerm) &&
+              //     title !== currentSearchTerm`,
+              //     currentSearchTerm &&
+              //       title.startsWith(currentSearchTerm) &&
+              //       title !== currentSearchTerm
+              //   );
+
               return (
                 currentSearchTerm &&
                 title.startsWith(currentSearchTerm) &&
                 title !== currentSearchTerm
               );
             })
-            .slice(0, 10)}
+            .slice(0, 10)
+            .map((item) => {
+              return (
+                <div key={item.id} onClick={() => setSearchTerm(item.title)}>
+                  {item.title}
+                </div>
+              );
+            })}
         </div>
       )}
     </div>
@@ -59,9 +104,3 @@ const Search = () => {
 };
 
 export default Search;
-
-const a = [1, 2, 3, 4, 5, 6, 7, 8];
-
-a.filter((item) => {
-  return item;
-}).slice(0, 10);
--- src/Component/SearchResult.jsx
+++ src/Component/SearchResult.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { callApi } from "../Utils/CallApi";
 
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  const [searchParams] = useSearchParams();
+  const [product, setProduct] = useState(null);
+
+  const getSearchResults = () => {
+    const searchterm = searchParams.get("searchterm");
+    console.log("searchterm=================>", searchterm);
+
+    const category = searchParams.get("category");
+    console.log("category=================>", category);
+
+    callApi(`data/search.json`).then((searchResults) => {
+      console.log("searchResults==========>", searchResults);
+
+      const categoryResult = searchResults[category];
+      console.log("categoryResult==========>", categoryResult);
+
+      if (searchterm) {
+        const result = categoryResult.filter((product) => {
+          return product.title
+            .toLowerCase()
+            .includes(searchterm.toLocaleLowerCase());
+        });
+        setProduct(result);
+      } else {
+        setProduct(categoryResult);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getSearchResults();
+  }, [searchParams]);
+
+  return (
+    <div className="min-w-[1200px] max-w-[1300px] m-auto">
+      {product &&
+        product.map((item, key) => {
+          return (
+            <div key={key}>
+              <p>{item.title}</p>
+              {/* <p>{item.brand}</p>
+              <img src={item.image} alt={item.image} /> */}
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default SearchResult;
